441c75e80fce73e2c81a270e27f196074d54a93a
cameron <cameron.studdstreet@gmail.com>
2022-03-17 10:46:49 +0000

    add license header

--- proptest/src/arbitrary/evm.rs
+++ proptest/src/arbitrary/evm.rs
@@ -1,3 +1,12 @@
+//-
+// Copyright 2022 The proptest developers
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 use primitive_types::{H128, H160, H256, H512, U128, U256, U512};
 
 use crate::strategy::Map;
